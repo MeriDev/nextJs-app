22f71a08ffb6c778e624bd4e46cf2f20b7b6c762
miapiar <meri.lacouturiere@gmail.com>
2024-06-08 00:12:29 +0100

    added route params

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,5 +1,9 @@
+import Link from 'next/link';
+
 const getTickets = async () => {
-  const res = await fetch('http://localhost:4000/tickets');
+  const res = await fetch('http://localhost:4000/tickets', {
+    next: { revalidate: 0 }, //to opt out of cashing
+  });
   return res.json();
 };
 
@@ -10,11 +14,13 @@ const TicketList = async () => {
     <>
       {tickets.map(ticket => (
         <div key={ticket.id} className="card my-5">
-          <h3>{ticket.title}</h3>
-          <p>{ticket.body.slice(0, 200)}...</p>
-          <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
       ))}
       {tickets.length === 0 && (
